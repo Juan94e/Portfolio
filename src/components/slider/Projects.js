886df3f415769bf18e95cf75ca9a4fcc74b5c75c
Juan E <juan94e@gmail.com>
2021-09-07 08:13:18 -0500

    Termino casi todo, ahora lo voy a subir a github pages y a firebase

--- src/components/slider/Projects.js
+++ src/components/slider/Projects.js
@@ -2,12 +2,17 @@ import React from 'react'
 import '../../styles/Projects.css';
 // ********** Images Import **********
 import licor from '../../assets/images/Capture.PNG';
-import flutter from '../../assets/images/lasTres1.jpg';
+import flutterApp from '../../assets/images/lasTres1.jpg';
 import portfolio from '../../assets/images/portfolio.PNG';
 // ********** Icons Import **********
 import html from '../../assets/Icons/html.png';
 import css from '../../assets/Icons/css.png';
 import javasc from '../../assets/Icons/javasc.png';
+import react from '../../assets/Icons/react.png';
+import flutter from '../../assets/Icons/flutter.png';
+import dart from '../../assets/Icons/dart.png';
+import firebase from '../../assets/Icons/firebase.png';
+
 
 
 export const Projects = () => {
@@ -25,45 +30,33 @@ export const Projects = () => {
                             Liquor Store
                         </h1>
                         <p>
-                            Working on:
+                            This Website contains a liquor store, you can see all liquors like
+                            wine, whisky, champagne, tequila and more. Actually not finished yet. working on
+                            Backend, DataBase and an Ecommerce.
                         </p>
-                        <ul>
-                            <li>
-                                Backend
-                            </li>
-                            <li>
-                                Liquor DataBase
-                            </li>
-                            <li>
-                                Liquor Search and Filtering
-                            </li>
-                            <li>
-                                Ecommerce
-                            </li>
-                        </ul>
                     </div>
                     <div className="manualCard manualBack">
-                        <h1> Technologies Used </h1>
-                        <div id="edu-icons-container2">
+                        <h1> Used Technologies </h1>
+                        <div id="projects-icons-container">
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon6" src={html} style={{width:"60px"}} alt="C programming" />
+                                    <img className="icon6" src={html} style={{width:"60px"}} alt="HTML" />
                                 </div>
                             </div>
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon7" src={css} style={{width:"60px"}} alt="Automation" />
+                                    <img className="icon7" src={css} style={{width:"60px"}} alt="CSS" />
                                 </div>
                             </div>
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon8" src={javasc} style={{width:"63px"}} alt="Arduino" />
+                                    <img className="icon8" src={javasc} style={{width:"63px"}} alt="JavaScript" />
                                 </div>
                             </div>
                         </div>
                         <div className="btn-container" >
-                            <a href="" className="manualBtn"> Github </a>
-                            <a href="" className="manualBtn"> Live Site </a>
+                            <a href="https://github.com/Juan94e/Liquor_Store" target="_blank" className="manualBtn"> Github </a>
+                            <a href="https://joao-sitee.web.app/" target="_blank" className="manualBtn"> Live Site </a>
                         </div>
                     </div>
                 </div>
@@ -72,42 +65,33 @@ export const Projects = () => {
                     <div className="manualCard manualFront">
                         <img className="card-image" src={portfolio} />
                         <h1>
-                            Liquor Store
+                            Portfolio
                         </h1>
                         <p>
-                            Working on:
+                            This is my own Website, here you can find info about me,  such as my education, knowledge, skills, languages and more. Get in touch...
                         </p>
-                        <ul>
-                            <li>
-                                Backend
-                            </li>
-                            <li>
-                                Liquor DataBase
-                            </li>
-                            <li>
-                                Liquor Search and Filtering
-                            </li>
-                            <li>
-                                Ecommerce
-                            </li>
-                        </ul>
                     </div>
                     <div className="manualCard manualBack">
-                        <h1> Technologies Used </h1>
-                        <div id="edu-icons-container2">
+                        <h1> Used Technologies </h1>
+                        <div id="projects-icons-container">
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon6" src={html} style={{width:"60px"}} alt="C programming" />
+                                    <img className="icon6" src={html} style={{width:"60px"}} alt="HTML" />
                                 </div>
                             </div>
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon7" src={css} style={{width:"60px"}} alt="Automation" />
+                                    <img className="icon7" src={css} style={{width:"60px"}} alt="CSS" />
                                 </div>
                             </div>
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon8" src={javasc} style={{width:"63px"}} alt="Arduino" />
+                                    <img className="icon8" src={javasc} style={{width:"63px"}} alt="JavaScript" />
+                                </div>
+                            </div>
+                            <div>
+                                <div className="react-icon-container">
+                                    <img className="react-icon" src={react} style={{width:"60px"}} alt="React" />
                                 </div>
                             </div>
                         </div>
@@ -120,50 +104,35 @@ export const Projects = () => {
 
                 <div className="manualContainer">
                     <div className="manualCard manualFront">
-                        <img className="card-image" src={flutter} />
+                        <img className="card-image" src={flutterApp} />
                         <h1>
-                            Liquor Store
+                            Flutter App
                         </h1>
                         <p>
-                            Working on:
+                            This is a mini application with email and google authentication. It is not completely finished yet, I am currently working on it
                         </p>
-                        <ul>
-                            <li>
-                                Backend
-                            </li>
-                            <li>
-                                Liquor DataBase
-                            </li>
-                            <li>
-                                Liquor Search and Filtering
-                            </li>
-                            <li>
-                                Ecommerce
-                            </li>
-                        </ul>
                     </div>
                     <div className="manualCard manualBack">
-                        <h1> Technologies Used </h1>
-                        <div id="edu-icons-container2">
+                        <h1> Used Technologies </h1>
+                        <div id="projects-icons-container">
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon6" src={html} style={{width:"60px"}} alt="C programming" />
+                                    <img className="icon6" src={dart} style={{width:"60px"}} alt="Dart" />
                                 </div>
                             </div>
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon7" src={css} style={{width:"60px"}} alt="Automation" />
+                                    <img className="icon7" src={flutter} style={{width:"60px"}} alt="Flutter" />
                                 </div>
                             </div>
                             <div>
                                 <div className="iconTagName">
-                                    <img className="icon8" src={javasc} style={{width:"63px"}} alt="Arduino" />
+                                    <img className="icon8" src={firebase} style={{width:"63px"}} alt="FireBase" />
                                 </div>
                             </div>
                         </div>
                         <div className="btn-container" >
-                            <a href="" className="manualBtn"> Github </a>
-                            <a href="" className="manualBtn"> Live Site </a>
+                            <a href="https://github.com/Juan94e/FlutterApp" target="_blank" className="manualBtn"> Github </a>
                         </div>
                     </div>
                 </div>
